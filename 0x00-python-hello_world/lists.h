e7eade2a997ee8b5e6b901112246c1977bc6752a
Dev-sande <tapiwasande9@gmail.com>
2023-05-08 05:35:30 -0700

    initial commit

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,18 +1,20 @@
-#ifndef LISTS
-#define LISTS
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
+
 /**
-* struct listint_s - function name -singly linked list
-* @n: integer
-* @next: points to the next node
-* Description: prints all the values stored in the list
-* and returns the number of nodes in the list.
-*/
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * 
+ */
 typedef struct listint_s
 {
-int n;
-struct listint_s *next;
+    int n;
+    struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -20,5 +22,4 @@ listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
-
+#endif /* LISTS_H */
