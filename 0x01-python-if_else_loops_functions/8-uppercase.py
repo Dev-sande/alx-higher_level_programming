cd8b639ba3d216cfb0eb366643314a3c2a5bc7a5
Dev-sande <tapiwasande9@gmail.com>
2023-05-09 16:08:04 -0700

    fixed syntax error

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,4 @@
+#!/usr/bin/python3
 def uppercase(s):
-    """Prints a string in uppercase"""
-    new_s = ""
-    for c in s:
-        if ord(c) >= 97 and ord(c) <= 122:
-            new_s += chr(ord(c) - 32)
-        else:
-            new_s += c
-    print("{:s}".format(new_s))
+    return ''.join(c.upper() for c in s)
+
