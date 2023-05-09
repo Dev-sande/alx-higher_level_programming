d3a1c2bfb780d98e5db41d26cb55daf408e324b3
Dev-sande <tapiwasande9@gmail.com>
2023-05-09 16:04:15 -0700

    initial commit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,6 @@
-#!/usr/bin/python3
 def islower(c):
-    return True if 'a' <= c <= 'z' else False
+    """Checks if a character is lowercase"""
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,3 +1,9 @@
-#!/usr/bin/python3
 def uppercase(s):
-    return ''.join(c.upper() for c in s)
+    """Prints a string in uppercase"""
+    new_s = ""
+    for c in s:
+        if ord(c) >= 97 and ord(c) <= 122:
+            new_s += chr(ord(c) - 32)
+        else:
+            new_s += c
+    print("{:s}".format(new_s))
