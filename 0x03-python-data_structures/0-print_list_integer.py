1f771a1aa04400b9fe653c39ed9a87627b185e0e
Dev-sande <tapiwasande9@gmail.com>
2023-05-15 07:02:49 -0700

    initial commit

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=None):
-    my_list = my_list or []
-    for item in my_list:
-        print(item)
+def print_list_integer(my_list=[]):
+
+    for a in range(len(my_list)):
+        print("{:d}".format(my_list[a]))
+
