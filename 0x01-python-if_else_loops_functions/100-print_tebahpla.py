2b6f6a954432d3cc9dcf8b23b3af2d61bec0502b
Dev-sande <tapiwasande9@gmail.com>
2023-05-18 11:44:37 +0000

    fixed pycode style

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+# 100-print_tebahpla.py
 
-for ch in range(122, 96, -1):
-    print(f"{chr(ch) if ch % 2 == 0 else chr(ch - 32)}", end='')
+""""Print the alphabet in reverse order alternating upper- and lower-case."""
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
+
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def uppercase(s):
     return ''.join(c.upper() for c in s)
-
