bb76622fdf5b4a0909c86aac2aa836b455080de0
Dev-sande <tapiwasande9@gmail.com>
2023-05-31 01:20:40 +0000

    initial commit

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+''' a class Square that defines a square
+'''
+
 
 class Square:
-    """a class Square that defines a square by: (based on 0-square.py)
-    """
+
+    ''' Private instance attribute: size
+    '''
+
     def __init__(self, size):
-        """Private instance attribute: size
-           Instantiation with size
-          (no type/value verification)    
-        """
         self.__size = size
