c18ab8264b0dcfeefcdec5abae3b7a6bf1d222bb
Dev-sande <tapiwasande9@gmail.com>
2023-08-28 18:18:18 +0200

    initial commit

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
+""" Start link class to table in database
 """
-
-"""
-from sys import argv
+import sys
 from relationship_state import Base, State
 from relationship_city import City
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import asc
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            sys.argv[1],
+            sys.argv[2],
+            sys.argv[3]),
+        pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State):
-        for city in state.cities:
-            print("{:d}: {} -> {}".format(city.id, city.name, state.name))
+    citis = session.query(City).order_by(City.id).all()
+    for city in citis:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
     session.close()
